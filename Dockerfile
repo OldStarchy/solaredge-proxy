4be166a4b7dd7e5e8cff8c16673bc0feb5328f58
Nicholas Sorokin <nicholas.sorokin@plastyk.com.au>
2022-02-24 18:36:16 +1030

    fix: handle interrupts for a graceful shutdown

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,16 @@
 FROM node:16-alpine
+RUN apk add dumb-init
+ENV NODE_ENV=production
 
 WORKDIR /app
 
-COPY . /app
+COPY package.json /app/package.json
+COPY yarn.lock /app/yarn.lock
+
 RUN yarn install --prod
 
-CMD ["yarn", "start"]
+COPY app.js /app/app.js
+
+USER node
+
+CMD ["dumb-init", "node", "app.js"]
--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-var proxy = require('express-http-proxy');
-var app = require('express')();
+const proxy = require('express-http-proxy');
+let app = require('express')();
 
 if (process.env.LOGLEVEL == 'debug') {
 	app.use((req, res, next) => {
 		console.log(`Request to ${req.url}`);
-		const result = next();
-
-		console.log(res.statusCode);
-		return result;
+		return next();
 	});
 }
 
-app.use('/', proxy('https://monitoringapi.solaredge.com', {}));
+app.use('/', proxy('https://monitoringapi.solaredge.com'));
 
 const port = process.env.PORT || 80;
-console.log(`Listening on port ${port}`);
-app.listen(port);
+console.log(
+	`Listening on port ${port} (this may be different depending on your docker port bindings)`
+);
+
+const server = app.listen(port);
+
+const exitNicely = () => {
+	console.info('Interrupted');
+	server.close();
+	process.exit(0);
+};
+
+process.on('SIGINT', exitNicely);
+process.on('SIGTERM', exitNicely);
